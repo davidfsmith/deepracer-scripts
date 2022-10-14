2f6b1cee4976a3d71f0c6df256ea48e2d2c97e45
David Smith <dasmthc@amazon.co.uk>
2022-10-14 18:38:07 +0100

    Added in GPU drvier install

--- usb-build.sh
+++ usb-build.sh
@@ -31,12 +31,12 @@ fi
 
 diskutil partitionDisk /dev/${disk} MBR fat32 BOOT 8gb exfat FLASH 16gb
 
+# Grab the zip -> https://s3.amazonaws.com/deepracer-public/factory-restore/Ubuntu20.04/BIOS-0.0.8/factory_reset.zip
+# Note if you have a smaller USB stick you'll need to unzip factory_reset.zip and copy the files across
+cp -R ./factory_reset/ /Volumes/FLASH
+
 # Grab the ISO -> https://s3.amazonaws.com/deepracer-public/factory-restore/Ubuntu20.04/BIOS-0.0.8/ubuntu-20.04.1-20.11.13_V1-desktop-amd64.iso
 # diskutil unmount /dev/${disk}s1
 # sudo dd if=./ubuntu-20.04.1-20.11.13_V1-desktop-amd64.iso of=/dev/${disk}s1 status=progress  <- didn't work
 # https://github.com/unetbootin/unetbootin/wiki/commands
 /Applications/unetbootin.app/Contents/MacOS/unetbootin method=diskimage isofile=ubuntu-20.04.1-20.11.13_V1-desktop-amd64.iso installtype=USB targetdrive=/dev/${disk}s1 autoinstall=yes
-
-# Grab the zip -> https://s3.amazonaws.com/deepracer-public/factory-restore/Ubuntu20.04/BIOS-0.0.8/factory_reset.zip
-# Note if you have a smaller USB stick you'll need to unzip factory_reset.zip and copy the files across
-cp -R ./factory_reset/ /Volumes/FLASH

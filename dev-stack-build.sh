545838ad360d7bb8acb8cf9df734f1919ab21c54
Lars Ludvigsen <larsll@outlook.com>
2022-10-28 18:22:51 +0000

    Compressed Image Support

--- dev-stack-build.sh
+++ dev-stack-build.sh
@@ -19,13 +19,23 @@ rosdep install -i --from-path . --rosdistro foxy -y
 
 # Update packages for PR's
 cd aws-deepracer-inference-pkg
-git fetch origin pull/3/head:device-param
-git checkout device-param
+git fetch origin pull/4/head:compressed-image
+git checkout compressed-image
 cd ..
 
 cd aws-deepracer-camera-pkg
-git fetch origin pull/3/head:fps
-git checkout fps
+git fetch origin pull/5/head:compressed-image
+git checkout compressed-image
+cd ..
+
+cd aws-deepracer-interfaces-pkg
+git fetch origin pull/4/head:compressed-image
+git checkout compressed-image
+cd ..
+
+cd aws-deepracer-sensor-fusion-pkg
+git fetch origin pull/4/head:compressed-image
+git checkout compressed-image
 cd ..
 
 #
@@ -156,7 +166,10 @@ def generate_launch_description():
         package='sensor_fusion_pkg',
         namespace='sensor_fusion_pkg',
         executable='sensor_fusion_node',
-        name='sensor_fusion_node'
+        name='sensor_fusion_node',
+        parameters=[{
+                'image_transport': 'compressed'
+            }]    
     )
     servo_node = Node(
         package='servo_pkg',

e8f2d8007306bcc2b6e5e580b4b5da90863a0414
Lars Lorentz Ludvigsen <larsll@outlook.com>
2022-12-01 00:13:13 +0000

    Fixing missing package

--- dev-stack-dependencies.sh
+++ dev-stack-dependencies.sh
@@ -3,7 +3,7 @@
 # https://github.com/aws-deepracer/aws-deepracer-launcher/blob/main/getting-started.md
 
 # Install pre-requisites
-sudo apt-get install -y python3-websocket python3-colcon-common-extensions python3-rosinstall
+sudo apt-get install -y python3-websocket python3-colcon-common-extensions python3-rosinstall ros-foxy-compressed-image-transport
 
 # Set the environment
 source /opt/ros/foxy/setup.bash 

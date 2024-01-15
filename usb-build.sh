225079fe5dab2c3deb7496b5aee219c4e837caf4
David Smith <dasmthc@amazon.co.uk>
2024-01-15 15:03:59 +0000

    Fix issue with the wifi-creds, re-ordering steps and status messages

--- usb-build.sh
+++ usb-build.sh
@@ -26,42 +26,57 @@ if [ $OPTIND -eq 1 ]; then
     usage
 fi
 
+echo -e -n "\nCfreating partitions on USB /dev/${disk}\n"
 diskutil partitionDisk /dev/${disk} MBR fat32 BOOT 4gb fat32 DEEPRACER 2gb exfat FLASH 14gb
 
-# Grab the zip -> https://s3.amazonaws.com/deepracer-public/factory-restore/Ubuntu20.04/BIOS-0.0.8/factory_reset.zip
-factoryResetURL=https://s3.amazonaws.com/deepracer-public/factory-restore/Ubuntu20.04/BIOS-0.0.8/factory_reset.zip
-if [ ! -d factory_reset ]; then
-    curl -O ${factoryResetURL}
-    unzip factory_reset.zip
-
-    # uncomment `# reboot` on lines 520 & 528 of `usb_flash.sh`
-    cp factory_reset/usb_flash.sh factory_reset/usb_flash.sh.bak
-    rm factory_reset/usb_flash.sh
-    cat factory_reset/usb_flash.sh.bak | sed -e "s/#reboot/reboot/g" > factory_reset/usb_flash.sh
-fi
-rsync -av --progress factory_reset/* /Volumes/FLASH
-
 # Grab the ISO -> https://s3.amazonaws.com/deepracer-public/factory-restore/Ubuntu20.04/BIOS-0.0.8/ubuntu-20.04.1-20.11.13_V1-desktop-amd64.iso
+echo -e -n "\nChecking if we have the Ubuntu ISO"
 isoFilename=ubuntu-20.04.1-20.11.13_V1-desktop-amd64.iso
 isoURL=https://s3.amazonaws.com/deepracer-public/factory-restore/Ubuntu20.04/BIOS-0.0.8/ubuntu-20.04.1-20.11.13_V1-desktop-amd64.iso
 if [ ! -f ${isoFilename} ]; then
+    echo -e -n "\n- ISO not found - downloading"
     curl -O ${isoURL}
+else
+    echo -e -n "\n- ISO found"
 fi
-
-# Issues with OSX Ventura -> https://github.com/unetbootin/unetbootin/issues/337
-# https://github.com/unetbootin/unetbootin/wiki/commands
-sudo /Applications/unetbootin.app/Contents/MacOS/unetbootin method=diskimage isofile=ubuntu-20.04.1-20.11.13_V1-desktop-amd64.iso installtype=USB targetdrive=/dev/${disk}s1 autoinstall=yes
 
 # Create wifi-creds.txt for auto network goodness
 if [ ${ssid} != NULL ] && [ ${wifiPass} != NULL ]; then
+    echo -e -n "\nWriting 'wifi-creds.txt'\n"
 
-    cat > /Volumes/DEEPRACER/wifi-creds.txt << EOF
+    sudo cat > /Volumes/DEEPRACER/wifi-creds.txt << EOF
 ssid: ${ssid}
 password: ${wifiPass}
 EOF
 
 fi
 
-echo "Done"
+# Issues with 10.13 and above -> https://github.com/unetbootin/unetbootin/issues/337
+# https://github.com/unetbootin/unetbootin/wiki/commands
+echo -e -n "\n- Writing ISO to USB - Password required for disk access"
+sudo /Applications/unetbootin.app/Contents/MacOS/unetbootin method=diskimage isofile=ubuntu-20.04.1-20.11.13_V1-desktop-amd64.iso installtype=USB targetdrive=/dev/${disk}s1 autoinstall=yes
+# sudo diskutil umount /dev/${disk}s1
+# sudo dd if=${isoFilename} of=/dev/${disk}s1 bs=1m status=progress
+
+# Grab the zip -> https://s3.amazonaws.com/deepracer-public/factory-restore/Ubuntu20.04/BIOS-0.0.8/factory_reset.zip
+echo -e -n "\nChecking if we have 'factory_reset.zip'\n"
+factoryResetURL=https://s3.amazonaws.com/deepracer-public/factory-restore/Ubuntu20.04/BIOS-0.0.8/factory_reset.zip
+if [ ! -d factory_reset ]; then
+    echo -e -n "\n- Zip file not found - downloading"
+    curl -O ${factoryResetURL}
+    unzip factory_reset.zip
+
+    # uncomment `# reboot` on lines 520 & 528 of `usb_flash.sh`
+    echo -e -n "\n- Updating 'usb_flash.sh' so the car reboots after install"
+    cp factory_reset/usb_flash.sh factory_reset/usb_flash.sh.bak
+    rm factory_reset/usb_flash.sh
+    cat factory_reset/usb_flash.sh.bak | sed -e "s/#reboot/reboot/g" > factory_reset/usb_flash.sh
+else
+    echo -e -n "\nZip file found"
+fi
+echo -e -n "\n- Writing zip file contents to USB\n"
+rsync -av --progress factory_reset/* /Volumes/FLASH
+
+echo -e -n "\nDone"
 
 exit 0
